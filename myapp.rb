f0415c9ef444df98aa0f085fc3d6759063ce4b9f
chikako hoshi <chikako.5656pp@gmail.com>
2017-12-14 09:30:23 +0000

    :q
    :q#	public/

---
+++ myapp.rb
@@ -0,0 +1,44 @@
+require 'sinatra'
+require 'sinatra/reloader'
+
+get '/' do
+  erb :myapp
+end
+
+get '/contacts' do
+  erb :contacts
+end
+
+get '/blogs' do
+   erb :index 
+end
+
+get '/a' do
+    erb :a
+end
+
+get '/b' do
+    erb :b
+end
+
+get '/c' do
+    erb :b
+end
+
+post '/contacts' do
+    @name = params['name']
+    @email = params['email']
+    @content = params['content']
+    erb :confirm
+end
+
+get '/users' do
+    erb :form
+end
+
+post '/users' do
+    @email = params['email']
+    @password = params['password']
+    @name = params['name']
+    erb :user_confirm
+end
